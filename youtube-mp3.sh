4372b38ad1514c6d53b26b188dadcbc3f21e5152
vinamarora8 <vinam.arora8@gmail.com>
2017-09-14 02:38:27 +0530

    Added -o and -f options

--- youtube-mp3.sh
+++ youtube-mp3.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 k=false;					# keep
+o=false;					# output filename
+f=false;					# force output_filename
+
 start_time=00:00:10;
 
 # Option handling
-while getopts t:k opts
+while getopts t:o:kf opts
 do	case "$opts" in
 	t)		if [[ $OPTARG =~ ^[0-9]+$ ]]; then 		# validity of argument
 				start_time=00:00:$OPTARG;
@@ -14,6 +17,11 @@ do	case "$opts" in
 			fi;;
 
 	k)		k=true;;
+
+	o)		o=true;
+			output_filename=$OPTARG;;
+
+	f)		f=true;;
 
 	[?])	echo "$0": Try again with valid options;
 			exit;;
@@ -25,10 +33,10 @@ link=${!OPTIND}
 title=`youtube-dl --no-warnings -e $link`;
 
 # Check if a file with that name exists
-if [ -f "$title".mp3 ]; then
+if [ -f "$title".mp3 ] && ! $f; then
 	echo "$0": File "$title".mp3 already exists.;
 	echo "$0": Cannot overwrite. Exiting...;
-	exit;
+	exit 1;
 fi
 
 # Download video, and extract audio using youtube-dl tool
@@ -41,6 +49,16 @@ ffmpeg -i "$title"0.webm -ss $start_time -vframes 1 "$title.jpg";
 lame --ti "$title.jpg" --noreplaygain "$title"0.mp3 "$title.mp3";
 
 # Remove temp files if 'k' flag is not set
-if [ $k = false ]; then
+if ! $k ; then
+	echo $0": Removing temporary files";
 	rm "$title"0.mp3 "$title"0.webm "$title.jpg";
 fi;
+
+# Rename the output file to output_filename
+if $o ; then
+	echo $0": Renaming file to "$output_filename;
+fi
+
+mv -f "$title".mp3 "$output_filename";
+
+exit 0;
