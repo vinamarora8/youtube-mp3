6c18b29a8b2129a5555e7a2c5e20e8f3ec9adbb1
vinamarora8 <vinam.arora8@gmail.com>
2017-09-14 02:25:14 +0530

    Fixed syntax error

--- youtube-mp3.sh
+++ youtube-mp3.sh
@@ -4,8 +4,8 @@ k=false;					# keep
 start_time=00:00:10;
 
 # Option handling
-while getopts t:k o
-do	case "$o" in
+while getopts t:k opts
+do	case "$opts" in
 	t)		if [[ $OPTARG =~ ^[0-9]+$ ]]; then 		# validity of argument
 				start_time=00:00:$OPTARG;
 			else
@@ -22,22 +22,25 @@ done
 
 # Get video name
 link=${!OPTIND}
-A=`youtube-dl --no-warnings -e $link`;
+title=`youtube-dl --no-warnings -e $link`;
 
 # Check if a file with that name exists
-if [ -f $A ]; then
-	echo "$0": "$A".mp3 already exists. Cannot overwrite. Exiting...;
+if [ -f "$title".mp3 ]; then
+	echo "$0": File "$title".mp3 already exists.;
+	echo "$0": Cannot overwrite. Exiting...;
+	exit;
+fi
 
 # Download video, and extract audio using youtube-dl tool
 youtube-dl -f webm -x --audio-format mp3 -o '%(title)s0.%(ext)s' -k $link;
 
 # Get the image to be used as album art
-ffmpeg -i "$A"0.webm -ss $start_time -vframes 1 "$A.jpg";
+ffmpeg -i "$title"0.webm -ss $start_time -vframes 1 "$title.jpg";
 
 # Put the image as album art
-lame --ti "$A.jpg" --noreplaygain "$A"0.mp3 "$A.mp3";
+lame --ti "$title.jpg" --noreplaygain "$title"0.mp3 "$title.mp3";
 
 # Remove temp files if 'k' flag is not set
 if [ $k = false ]; then
-	rm "$A"0.mp3 "$A"0.webm "$A.jpg";
+	rm "$title"0.mp3 "$title"0.webm "$title.jpg";
 fi;
